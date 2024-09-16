5d3a8e4e6dcbe011abbc97d20b1af1f5cc63487f
NEO <younesmailbox1@gmail.com>
2024-09-16 11:00:19 +0100

    LOGIN endpoin || (backend-only)

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -11,7 +11,6 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-
 router.post("/verify-email", verifyEmail);
 
 export default router;
