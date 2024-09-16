5a790b9dbb68d5a0d2f7608962ed34ed39e11289
NEO <younesmailbox1@gmail.com>
2024-09-16 13:30:08 +0100

    FORGOT-PASSWORD endpoint || (Backendonly)

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,13 @@
-import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import crypto from "crypto";
+
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
-import { sendVerificationEmail, sendWelcomeEmail } from "../mailtrap/emails.js";
+import {
+  sendPasswordResetEmail,
+  sendVerificationEmail,
+  sendWelcomeEmail,
+} from "../mailtrap/emails.js";
+import { User } from "../models/user.model.js";
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
@@ -117,6 +122,7 @@ export const login = async (req, res) => {
       },
     });
   } catch (error) {
+    console.log("Error in login ", error);
     res.status(400).json({ success: false, message: error.message });
   }
 };
@@ -124,4 +130,40 @@ export const login = async (req, res) => {
 export const logout = async (req, res) => {
   res.clearCookie("token");
   res.status(200).json({ success: true, message: "Logged out Successfully" });
+};
+
+export const forgotPassword = async (req, res) => {
+  const { email } = req.body;
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found" });
+    }
+
+    // Generate reset token
+    const resetToken = crypto.randomBytes(20).toString("hex");
+    const resetTokenExpiresAt = Date.now() + 1 * 60 * 60 * 1000; // 1 hour
+
+    user.resetPasswordToken = resetToken;
+    user.resetPasswordExpiresAt = resetTokenExpiresAt;
+
+    await user.save();
+
+    // send email
+    await sendPasswordResetEmail(
+      user.email,
+      `${process.env.CLIENT_URL}/reset-password/${resetToken}`
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Password reset link sent to your email",
+    });
+  } catch (error) {
+    console.log("Error in forgotPassword ", error);
+    res.status(400).json({ success: false, message: error.message });
+  }
 };
--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -1,5 +1,7 @@
-import { MailtrapClient } from "mailtrap";
-import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
+import {
+  PASSWORD_RESET_REQUEST_TEMPLATE,
+  VERIFICATION_EMAIL_TEMPLATE,
+} from "./emailTemplates.js";
 import { mailtrapClient, sender } from "./mailtrap.config.js";
 
 export const sendVerificationEmail = async (email, verificationToken) => {
@@ -23,9 +25,6 @@ export const sendVerificationEmail = async (email, verificationToken) => {
   }
 };
 
-// for tomorrow work
-
-// After Registering The User will recive an Email provide that the Account Is Verified
 export const sendWelcomeEmail = async (email, name) => {
   const recipient = [{ email }];
 
@@ -45,5 +44,23 @@ export const sendWelcomeEmail = async (email, name) => {
     console.error(`Error sending welcome email`, error);
 
     throw new Error(`Error sending welcome email: ${error}`);
+  }
+};
+
+export const sendPasswordResetEmail = async (email, resetURL) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Reset your password",
+      html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetURL),
+      category: "Password Reset",
+    });
+  } catch (error) {
+    console.error(`Error sending password reset email`, error);
+
+    throw new Error(`Error sending password reset email: ${error}`);
   }
 };
